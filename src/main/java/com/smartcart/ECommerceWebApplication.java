109ac38afa6e1e038c2e0672053ee0749a5f27dc
koti23223 <ramakotisayempu2@gmail.com>
2026-04-01 10:25:11 +0530

    changes

--- src/main/java/com/smartcart/ECommerceWebApplication.java
+++ src/main/java/com/smartcart/ECommerceWebApplication.java
@@ -1,14 +1,22 @@
 package com.smartcart;
 
+import com.smartcart.rest.OrderController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ECommerceWebApplication {
 
+    private final OrderController orderController;
+
+    ECommerceWebApplication(OrderController orderController) {
+        this.orderController = orderController;
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceWebApplication.class, args);
 		System.out.println("Rama koti");
+		System.err.println("Error");
 	}
 
 }
