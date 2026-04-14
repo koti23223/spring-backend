a60de4781e969c7cfe8554f4b4103668d9dda3f1
koti23223 <ramakotisayempu2@gmail.com>
2026-04-14 10:23:19 +0530

    deployed

--- src/main/java/com/smartcart/config/CorsConfig.java
+++ src/main/java/com/smartcart/config/CorsConfig.java
@@ -13,14 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:5173")
-//                		.allowedOrigins("*")
-                		.allowedOrigins("https://lifestyle23.netlify.app")
-                		
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+            	registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
             }
         };
     }
